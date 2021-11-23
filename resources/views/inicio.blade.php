d19b25949bf04c72d1b2570fc275e2707e423136
JORGE PARRA HIDALGO <19308051220709@cbtis122.edu.mx>
2021-11-23 01:53:22 +0000

    Initial commit from PHPSandbox

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enfermeria 122</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body style="background-color:#ADD1FE;">
+
+<h1 class="bg-blue-900 text-6xl text-white text-center">CBTis122 Registro Alumnos Enfermeria</h1>
+
+    <br><br>
+
+    
+
+    <!–– PRIMERO ––>
+    <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl mt-3 border">
+        <div class="flex-shrink-0">
+            <img class="h-12 w-12"
+                src="https://png.pngtree.com/png-clipart/20200401/original/pngtree-hospital-icon-design-illustration-png-image_5339806.jpg"
+                alt="ChitChat Logo">
+        </div>
+        <div class="ml-6 pt-1">
+            <h4 class="text-xl text-gray-900 leading-tight">Listado de alumnos regulares</h4>
+            <p class="text-base text-gray-600 leading-normal">Registros enfermeria</p>
+        </div>
+    </div>
+
+    <div class="container mx-auto mt-5 text-center">
+        <button class='bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700'>
+            Ir
+        </button>
+    </div>
+
+    <!–– SEGUNDO ––>
+    <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl mt-3 border">
+        <div class="flex-shrink-0">
+            <img class="h-12 w-12"
+                src="https://cdn-icons-png.flaticon.com/512/414/414323.png"
+                alt="ChitChat Logo">
+        </div>
+        <div class="ml-6 pt-1">
+            <h4 class="text-xl text-gray-900 leading-tight">Listado de alumnos con encesidades diferentes</h4>
+            <p class="text-base text-gray-600 leading-normal">Registros enfermeria</p>
+        </div>
+    </div>
+
+    <div class="container mx-auto mt-5 text-center">
+        <button class='bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700'>
+            Ir
+        </button>
+    </div>
+
+    <!–– TERCERO ––>
+    <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl mt-3 border">
+        <div class="flex-shrink-0">
+            <img class="h-12 w-12"
+                src="https://cdn-icons-png.flaticon.com/512/2892/2892041.png"
+                alt="ChitChat Logo">
+        </div>
+        <div class="ml-6 pt-1">
+            <h4 class="text-xl text-gray-900 leading-tight">Alumnos en riesgo</h4>
+            <p class="text-base text-gray-600 leading-normal">Registros enfermeria</p>
+        </div>
+    </div>
+
+    <div class="container mx-auto mt-5 text-center">
+        <button class='bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700'>
+            Ir
+        </button>
+    </div>
+
+    <!–– CUARTO ––>
+    <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl mt-3 border">
+        <div class="flex-shrink-0">
+            <img class="h-12 w-12"
+                src="https://cdn-icons-png.flaticon.com/512/2659/2659980.png"
+                alt="ChitChat Logo">
+        </div>
+        <div class="ml-6 pt-1">
+            <h4 class="text-xl text-gray-900 leading-tight">Contagios de covid</h4>
+            <p class="text-base text-gray-600 leading-normal">Registros enfermeria</p>
+        </div>
+    </div>
+
+    <div class="container mx-auto mt-5 text-center">
+        <button class='bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700'>
+            Ir
+        </button>
+    </div>
+
+    <!–– Quinto ––>
+    <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl mt-3 border">
+        <div class="flex-shrink-0">
+            <img class="h-12 w-12"
+                src="https://cdn-icons.flaticon.com/png/512/1850/premium/1850302.png?token=exp=1637631865~hmac=2de7dd92cbd5cc63bac67ccab1444f44"
+                alt="ChitChat Logo">
+        </div>
+        <div class="ml-6 pt-1">
+            <h4 class="text-xl text-gray-900 leading-tight">Alumnos con piojos</h4>
+            <p class="text-base text-gray-600 leading-normal">Registros enfermeria</p>
+        </div>
+    </div>
+
+    <div class="container mx-auto mt-5 text-center">
+        <button class='bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700'>
+            Ir
+        </button>
+    </div>
+
+
+
+
+
+    <br><br>
+
+    <div class="mt-5 border mx-5 p-10">
+    <p>
+        Pagina creada para ayudar y contribuir con el departamento de enfermeria del plantel academico CBTis122.
+        Creada por Arath Ernesto Moreno Amador, Eric Guillermo Ramirez Barraza y Jorge Parra Hidalgo del Quinto semestre grupo F.
+    </p>
+</div>
+
+
+
+
+<h1 class="rounded-lg text-6xl text-white text-center bg-red-900 mx-5">Copyright © Todos los derechos reservados</h1>
+
+
+
+</body>
+
+</html>
